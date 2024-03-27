8e41c5047e22f5995704c1c1b535f120cd474f5e
Grace Chilton <gchilton@luc.edu>
2024-03-27 15:23:58 -0500

    Update filter script to call SearchSRA ToolKit GitHub

--- filter_reads.sh
+++ filter_reads.sh
@@ -2,30 +2,9 @@
 
 ## WORKING SCRIPT TO FILTER SEARCHSRA OUTPUT DATA ##
 
-set -euo pipefail
-results="results"
-zipfile="results.zip"
-mapq=3
-length=50
-verbose=n
-version=0.1
-outdir="./"
+# clone searchSRA ToolKit Github
+git clone https://github.com/linsalrob/SearchSRAToolKit.git
 
-usage=$(cat <<-EOF
-$0
-Version $version
-
-Please provide one of either:
--z --zip      The path to the file (usually called results.zip) that you downloaded from SearchSRA
--r --results  The directory with the uncompressed results (e.g. if you have extracted results.zip)
--o --outdir   The directory to write the results (default: $outdir)
-
--l --length   Minimum alignment length for the sequences to keep the alignment. Default: $length
--m --mapq     Minimum MAPQ (mapping quality) score to keep the alignment. Default $mapq
-
--v --verbose  More output
-
--h --help     Print this message and exit
-
-EOF
-)
+#run filter reads script from toolkit
+#filters for matches with a MAPQ alignment >= 20, and an alignment >= 100 nucleotides
+./filter_reads.sh -m 20 -l 100 -v -o FILTERED
