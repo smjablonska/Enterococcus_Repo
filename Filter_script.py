3a99de041a8fc53f37f90431fdf8881e1a996587
Grace Chilton <gchilton@luc.edu>
2024-04-29 11:06:53 -0500

    Update filtering script

--- Filter_script.py
+++ Filter_script.py
@@ -1,11 +1,12 @@
 # Filtering script
 import pandas as pd
-df = pd.read_csv("ARA-main/results/results/Arabidopsis_thaliana.TAIR10.ncrna/Arabidopsis_thaliana.TAIR10.ncrna.screening.analysis.stats.sorted.by.alignment.txt", sep="\t")
-
+df = pd.read_csv("ARA-main/MZ.screening.analysis.stats.sorted.by.alignment.txt",sep="\t")
 df = pd.DataFrame(df)
 # Filtering data on alignment rate in Bowtie2
 # Only keeping alignment rates above 70%
-filtered = df[df['Unique_hits_perc_in_Bowtie2'] > 70]
-filtered = filtered[['SRA_Run_ID','Unique_hits_perc_in_Bowtie2']]
-
+df['Unique_hits_perc_in_Bowtie2'] = df['Unique_hits_perc_in_Bowtie2'] != '-'
+df['Unique_hits_perc_in_Bowtie2'] = df['Unique_hits_perc_in_Bowtie2'].astype(float)
+filtered = df[df['Unique_hits_perc_in_Bowtie2'] > 0]
+filtered = filtered[['SRA_Run_ID','Unique_hits_perc_in_Bowtie2','isolation_source']]
+# Writing to file including SRA id accession number and tissue sampled for abundancy analysis
 filtered.to_csv("Filtered_data.csv",sep=",")
