7cd0376eb7799d97334eac678e048c4033f1ca4f
Grace Chilton <gchilton@luc.edu>
2024-04-29 10:39:34 -0500

    Subsetting filtered data

--- Filter_script.py
+++ Filter_script.py
@@ -1,10 +1,11 @@
 # Filtering script
-# Getting rid of entries with 0 hits
-
 import pandas as pd
 df = pd.read_csv("ARA-main/results/results/Arabidopsis_thaliana.TAIR10.ncrna/Arabidopsis_thaliana.TAIR10.ncrna.screening.analysis.stats.sorted.by.alignment.txt", sep="\t")
 
 df = pd.DataFrame(df)
-filtered = df[df['Unique_hits_perc_in_Bowtie2'] > 0]
+# Filtering data on alignment rate in Bowtie2
+# Only keeping alignment rates above 70%
+filtered = df[df['Unique_hits_perc_in_Bowtie2'] > 70]
+filtered = filtered[['SRA_Run_ID','Unique_hits_perc_in_Bowtie2']]
 
-print(filtered['Unique_hits_perc_in_Bowtie2'])
+filtered.to_csv("Filtered_data.csv",sep=",")
