9ecb6bff5868998bc142c1c90b07cf47788c37c6
Grace Chilton <gchilton@luc.edu>
2024-04-30 08:01:00 -0500

    Updated filtering script

--- Filter_script.py
+++ Filter_script.py
@@ -3,9 +3,8 @@ import pandas as pd
 df = pd.read_csv("CP.screening.analysis.stats.sorted.by.alignment.txt",sep="\t")
 df = pd.DataFrame(df)
 # Filtering data on alignment rate in Bowtie2
-# Only keeping alignment rates above 70%
-print(df['Unique_hits_perc_in_Bowtie2'])
-#df['Unique_hits_perc_in_Bowtie2'] = df['Unique_hits_perc_in_Bowtie2'] != '-'
+# Only keeping unique hit percentages above 1.0
+
 df = df.drop(df[df['Unique_hits_perc_in_Bowtie2'] == '-'].index)
 df['Unique_hits_perc_in_Bowtie2'] = df['Unique_hits_perc_in_Bowtie2'].astype(float)
 filtered = df[df['Unique_hits_perc_in_Bowtie2'] >= 1.000]
