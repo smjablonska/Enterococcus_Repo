1c06e5aa6c17831a2bb37fe05579decb8fa39f68
Grace Chilton <gchilton@luc.edu>
2024-04-30 07:59:08 -0500

    Updated filtering script

--- Filter_script.py
+++ Filter_script.py
@@ -1,12 +1,14 @@
 # Filtering script
 import pandas as pd
-df = pd.read_csv("ARA-main/MZ.screening.analysis.stats.sorted.by.alignment.txt",sep="\t")
+df = pd.read_csv("CP.screening.analysis.stats.sorted.by.alignment.txt",sep="\t")
 df = pd.DataFrame(df)
 # Filtering data on alignment rate in Bowtie2
 # Only keeping alignment rates above 70%
-df['Unique_hits_perc_in_Bowtie2'] = df['Unique_hits_perc_in_Bowtie2'] != '-'
+print(df['Unique_hits_perc_in_Bowtie2'])
+#df['Unique_hits_perc_in_Bowtie2'] = df['Unique_hits_perc_in_Bowtie2'] != '-'
+df = df.drop(df[df['Unique_hits_perc_in_Bowtie2'] == '-'].index)
 df['Unique_hits_perc_in_Bowtie2'] = df['Unique_hits_perc_in_Bowtie2'].astype(float)
-filtered = df[df['Unique_hits_perc_in_Bowtie2'] > 0]
+filtered = df[df['Unique_hits_perc_in_Bowtie2'] >= 1.000]
 filtered = filtered[['SRA_Run_ID','Unique_hits_perc_in_Bowtie2','isolation_source']]
 # Writing to file including SRA id accession number and tissue sampled for abundancy analysis
 filtered.to_csv("Filtered_data.csv",sep=",")
